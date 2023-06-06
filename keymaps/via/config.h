8e791a71bffdea3b33206c2ca927b31ddc25b5b9
oddlyspaced <oddlyspaced1@gmail.com>
2023-06-06 17:32:34 +0530

    keymaps/via: set oled brightness to be 50%

--- keymaps/via/config.h
+++ keymaps/via/config.h
@@ -24,4 +24,5 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 // #define MASTER_RIGHT
 // #define EE_HANDS
 
+#define OLED_BRIGHTNESS 128
 #define OLED_FONT_H "keyboards/crkbd/lib/glcdfont.c"
